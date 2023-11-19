45888d7aeba39224ea6aaedfafe4a41eab67f411
nithika987 <150774916+nithika987@users.noreply.github.com>
2023-11-19 08:40:03 +0530

    home page

--- src/components/Home/Sidebar.js
+++ src/components/Home/Sidebar.js
@@ -9,7 +9,7 @@ const Sidebar = () => {
           <a href="#">Calendar</a>
         </li>
         <li>
-          <a href="#">Profile</a>
+          <a href="Profile.js">Profile</a>
         </li>
         <li>
           <a href="#">Wishlist</a>
